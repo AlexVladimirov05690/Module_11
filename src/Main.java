bb819d8b251c0619ef2d9895ab13e656c6df9380
AlexVladimirov05690 <93385788+AlexVladimirov05690@users.noreply.github.com>
2022-01-21 22:30:21 +0300

    Finish commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         CreateChampionship createChampionship = new CreateChampionship();
         createChampionship.addTeams();
         createChampionship.championship();
-        List<Team> teamList = new ArrayList<>(createChampionship.teams.values());
-        for(int i = 0; i < teamList.size(); i++) {
-            System.out.println(teamList.get(i).toString());
+        for(int i = 1; i <= createChampionship.teams.size(); i++) {
+            System.out.println(createChampionship.teams.get(i));
         }
     }
 }
